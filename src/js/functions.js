3eaf12ffd8087a265acc5e00dc95663aac424473
Nadiia-Yuce <soyka9616@gmail.com>
2024-07-11 20:30:10 +0200

    add to local storage

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,6 +1,9 @@
-export function addBablo() { // створюємо функцію для обробки прослуховувача кліку кнопки
-    const countSpan = document.querySelector('#count'); // витягуємо елемент спану у змінну
-    let count = Number(countSpan.innerHTML); // витягуємо поточне значення спану
-    count++; // збільшуємо значення змінної count на 1
-    countSpan.innerHTML = count; // змінюємо контент спану на значення count 
+export function addBablo() {
+  // створюємо функцію для обробки прослуховувача кліку кнопки
+  const countSpan = document.querySelector('#count'); // витягуємо елемент спану у змінну
+  let count = Number(countSpan.innerHTML); // витягуємо поточне значення спану
+  count++; // збільшуємо значення змінної count на 1
+  countSpan.innerHTML = count; // змінюємо контент спану на значення count
+  const key = 'LSkey'; // Додаємо ключ для локал сторедж
+  localStorage.setItem(key, JSON.stringify(count)); //Додаємо в сторедж значення count (поточне значення спану)
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { addBablo } from './js/functions'; // імпортуємо функцію прослуховувача
 
 const button = document.querySelector('.button'); // витягуємо елемент кнопки для додавання прослуховувача
+const countSpan = document.querySelector('#count'); // витягуємо елемент спану у змінну
+
+const key = 'LSkey'; // Додаємо ключ для локал сторедж
+const savedInfo = localStorage.getItem(key); //дістаємо значення із стореджу
+if (savedInfo) {
+  //якщо сторедж не пустий, то...
+  countSpan.innerHTML = JSON.parse(savedInfo); //розпарсили значення стореджу і перезаписали значення спану
+}
 
 button.addEventListener('click', addBablo); // додаємо прослуховувач по кліку
