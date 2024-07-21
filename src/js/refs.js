b8bd2e9cf0572b8ee2e615b141754f4a48984a24
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-07-21 23:40:28 +0300

    Add missing files
    Command `git commit -a` does not add new files to the commit

---
+++ src/js/refs.js
@@ -0,0 +1,4 @@
+export default {
+  button: document.querySelector('.button'), // витягуємо елемент кнопки
+  countSpan: document.querySelector('#count'), // витягуємо елемент спану
+};
---
+++ src/js/variables.js
@@ -0,0 +1,4 @@
+const axiosURL = 'https://668581e2b3f57b06dd4cf9fd.mockapi.io/api/v1'; //задаємо базову адресу нашого бекенду для аксіос
+const LS_KEY = 'UserID'; //задаємо ключ для локал сторедж
+
+export { axiosURL, LS_KEY };
